db143a589f039729285329b16c3d92bea867083c
Anna Bendahan <an.bendahan@gmail.com>
2019-01-23 15:30:05 +0100

    client included

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 
-
-
-
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
   scope '/api' do
-    root to: "courses#index"
       resources :courses
   end
 end
