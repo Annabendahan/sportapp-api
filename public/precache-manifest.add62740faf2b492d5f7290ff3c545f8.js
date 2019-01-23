6f8588dd37f587a01b184a27a039b6f2d900de19
Anna Bendahan <an.bendahan@gmail.com>
2019-01-23 18:08:44 +0100

    test3

---
+++ public/precache-manifest.add62740faf2b492d5f7290ff3c545f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "146af0b86643447d3d4b",
+    "url": "/static/js/main.146af0b8.chunk.js"
+  },
+  {
+    "revision": "b1e95c70ef2e27ffada0",
+    "url": "/static/js/1.b1e95c70.chunk.js"
+  },
+  {
+    "revision": "146af0b86643447d3d4b",
+    "url": "/static/css/main.8a4bde3a.chunk.css"
+  },
+  {
+    "revision": "eb0f9b116758a31fc95405cfb375a3e1",
+    "url": "/index.html"
+  }
+];
